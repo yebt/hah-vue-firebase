41811270868b7c41145975cffd4515adc09f152f
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-15 02:28:39 -0500

    feat(packages): install pinia colada for better actions and queries

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@/assets/styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { PiniaColada } from '@pinia/colada'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +11,9 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PiniaColada,{
+// settings
+})
 app.use(router)
 
 app.mount('#app')
